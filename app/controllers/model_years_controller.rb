5cf60509f37008752b8afe55e7dd64a1b0a81792
JD <johndavid400@gmail.com>
2013-08-03 13:29:57 -0500

    adding modals

--- app/controllers/model_years_controller.rb
+++ app/controllers/model_years_controller.rb
@@ -1,6 +1,8 @@
 class ModelYearsController < ApplicationController
 
   def index
+    @make = params["make"]
+    @model = params["model"]
     @model_years = Edmunds::ModelYear.new.for_model_id(params[:id])
   end
 
